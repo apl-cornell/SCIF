383dc9fe22499726bc7bac6444af46f1bf3d120d
Ethan Cecchetti <ebc65@cornell.edu>
2020-12-01 17:30:07 -0500

    adding key-value store example

--- time_SCIFex_payable.sh
+++ time_SCIFex_payable.sh
@@ -1,11 +1,34 @@
 #!/bin/sh
 
-time ./stc -t -i=SCIFex/sendImp/BaseContract.scif -i=SCIFex/sendImp/DistributedBankEx.scif
-time ./stc -t -i=SCIFex/sendImp/BaseContract.scif -i=SCIFex/sendImp/DistributedBankExA0.scif
-time ./stc -t -i=SCIFex/sendImp/BaseContract.scif -i=SCIFex/sendImp/DistributedBankExW0.scif
-time ./stc -t -i=SCIFex/sendImp/BaseContract.scif -i=SCIFex/sendImp/Holder.scif -i=SCIFex/sendImp/TokenEx.scif -i=SCIFex/sendImp/UniswapEx.scif
-time ./stc -t -i=SCIFex/sendImp/BaseContract.scif -i=SCIFex/sendImp/Holder.scif -i=SCIFex/sendImp/TokenExA0.scif -i=SCIFex/sendImp/UniswapEx.scif
-time ./stc -t -i=SCIFex/sendImp/BaseContract.scif -i=SCIFex/sendImp/Holder.scif -i=SCIFex/sendImp/TokenExW0.scif -i=SCIFex/sendImp/UniswapEx.scif
-time ./stc -t -i=SCIFex/sendImp/BaseContract.scif -i=SCIFex/sendImp/Callback.scif -i=SCIFex/sendImp/TownCrierEx.scif
-time ./stc -t -i=SCIFex/sendImp/BaseContract.scif -i=SCIFex/sendImp/Callback.scif -i=SCIFex/sendImp/TownCrierExW0.scif
-time ./stc -t -i=SCIFex/sendImp/BaseContract.scif -i=SCIFex/sendImp/Callback.scif -i=SCIFex/sendImp/TownCrierExW1.scif
+echo -n "Multi-DAO 1: " >&2;
+time -f %U ./stc -t -i=SCIFex/sendImp/BaseContract.scif -i=SCIFex/sendImp/DistributedBankEx.scif
+
+echo -n "Multi-DAO 2: " >&2;
+time -f %U ./stc -t -i=SCIFex/sendImp/BaseContract.scif -i=SCIFex/sendImp/DistributedBankExA0.scif
+
+echo -n "Multi-DAO*: " >&2;
+time -f %U ./stc -t -i=SCIFex/sendImp/BaseContract.scif -i=SCIFex/sendImp/DistributedBankExW0.scif
+
+echo -n "Uniswap 1: " >&2;
+time -f %U ./stc -t -i=SCIFex/sendImp/BaseContract.scif -i=SCIFex/sendImp/Holder.scif -i=SCIFex/sendImp/TokenEx.scif -i=SCIFex/sendImp/UniswapEx.scif
+
+echo -n "Uniswap 2: " >&2;
+time -f %U ./stc -t -i=SCIFex/sendImp/BaseContract.scif -i=SCIFex/sendImp/Holder.scif -i=SCIFex/sendImp/TokenExA0.scif -i=SCIFex/sendImp/UniswapEx.scif
+
+echo -n "Uniswap*: " >&2;
+time -f %U ./stc -t -i=SCIFex/sendImp/BaseContract.scif -i=SCIFex/sendImp/Holder.scif -i=SCIFex/sendImp/TokenExW0.scif -i=SCIFex/sendImp/UniswapEx.scif
+
+echo -n "Town Crier: " >&2;
+time -f %U ./stc -t -i=SCIFex/sendImp/BaseContract.scif -i=SCIFex/sendImp/Callback.scif -i=SCIFex/sendImp/TownCrierEx.scif
+
+echo -n "Town Crier 1*: " >&2;
+time -f %U ./stc -t -i=SCIFex/sendImp/BaseContract.scif -i=SCIFex/sendImp/Callback.scif -i=SCIFex/sendImp/TownCrierExW0.scif
+
+echo -n "Town Crier 2*: " >&2;
+time -f %U ./stc -t -i=SCIFex/sendImp/BaseContract.scif -i=SCIFex/sendImp/Callback.scif -i=SCIFex/sendImp/TownCrierExW1.scif
+
+echo -n "KV Store: " >&2;
+time -f %U ./stc -t -i=SCIFex/map/Array.scif -i=SCIFex/map/MappingFunction.scif -i=SCIFex/map/Map.scif
+
+echo -n "KV Store*: " >&2;
+time -f %U ./stc -t -i=SCIFex/map/Array.scif -i=SCIFex/map/MappingFunction.scif -i=SCIFex/map/MapW0.scif
