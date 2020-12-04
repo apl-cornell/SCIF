7fa629340b46130f07b6b2a6bab7a8d50f27d8f4
Siqiu Yao <yaosiqiu@gmail.com>
2020-12-03 23:03:56 -0500

    TownCrier Full version added

--- time_SCIFex_payable.sh
+++ time_SCIFex_payable.sh
@@ -32,3 +32,12 @@ time -f %U ./stc -t -i=SCIFex/map/Array.scif -i=SCIFex/map/MappingFunction.scif 
 
 echo -n "KV Store*: " >&2;
 time -f %U ./stc -t -i=SCIFex/map/Array.scif -i=SCIFex/map/MappingFunction.scif -i=SCIFex/map/MapW0.scif
+
+echo -n "Town Crier FullVer 1: " >&2;
+time -f %U ./stc -t -i=SCIFex/sendImp/BaseContract.scif -i=SCIFex/sendImp/Callback.scif -i=SCIFex/sendImp/Crypto.scif -i=./SCIFex/sendImp/TownCrierExFull.scif
+
+echo -n "Town Crier FullVer 2*: " >&2;
+time -f %U ./stc -t -i=SCIFex/sendImp/BaseContract.scif -i=SCIFex/sendImp/Callback.scif -i=SCIFex/sendImp/Crypto.scif -i=./SCIFex/sendImp/TownCrierExFullW0.scif
+
+echo -n "Town Crier FullVer 3*: " >&2;
+time -f %U ./stc -t -i=SCIFex/sendImp/BaseContract.scif -i=SCIFex/sendImp/Callback.scif -i=SCIFex/sendImp/Crypto.scif -i=./SCIFex/sendImp/TownCrierExFullW1.scif
