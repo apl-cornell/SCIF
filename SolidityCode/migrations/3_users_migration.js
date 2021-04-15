87d07e2698e6e01680cc675593db2180ec9a287b
Siqiu Yao <yaosiqiu@gmail.com>
2021-04-14 20:49:45 -0400

    change directory name

--- SolidityCode/migrations/3_users_migration.js
+++
@@ -1,7 +0,0 @@
-var userContract = artifacts.require("BaseContractCentralized");
-var trustOracle = artifacts.require("TrustOracle");
-var lockOracle = artifacts.require("LockOracle");
-
-module.exports = function (deployer) {
-    deployer.deploy(userContract, trustOracle.address, lockOracle.address);
-};
---
+++ solidityCode/migrations/3_users_migration.js
@@ -0,0 +1,11 @@
+var trustOracle = artifacts.require("TrustOracle");
+var lockOracle = artifacts.require("LockOracle");
+var alice = artifacts.require("BaseContractCentralized");
+var bob = artifacts.require("BaseContractCentralized");
+var carl = artifacts.require("BaseContractCentralized");
+
+module.exports = function (deployer) {
+    // deployer.deploy(alice, trustOracle.address, lockOracle.address);
+    // deployer.deploy(bob, trustOracle.address, lockOracle.address);
+    // deployer.deploy(carl, trustOracle.address, lockOracle.address);
+};
