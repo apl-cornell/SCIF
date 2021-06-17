b9f1c5160efe2cbd1e427a9011b6f2d91ff7e06e
Siqiu Yao <yaosiqiu@gmail.com>
2021-06-17 08:48:53 -0400

    Wallet example passes.

--- SCIFex/Wallet.java
+++ SCIFex/Wallet.java
@@ -1,26 +1,21 @@
 contract Wallet [this] {
-    uint{this} balance;
     map(address, uint){this} balances;
 
-    bool init{BOT >> this; BOT}() {
-    }
-
-    void withdraw{BOT >> this; BOT}(address{BOT} sender, uint{BOT} amount) {
-
-        // ... check if this is the sender
-
-        address{this} gSender = endorse(sender, BOT->this);
+    @public
+    void withdraw{BOT >> this; this}(uint{BOT} amount) {
         uint{this} gAmount = endorse(amount, BOT->this);
 
-        if{this} (balances[gSender] >= gAmount && balance >= gAmount) {
-            balances[gSender] = balances[gSender] - gAmount;
-            balance = balance - gAmount;
-            send(gSender, gAmount);
+        lock(this) {
+            if{this} (balances[msg.sender] >= gAmount) {
+                balances[msg.sender] = balances[msg.sender] - gAmount;
+                send(msg.sender, gAmount);
+            }
         }
     }
 
-    bool decBal{this >> this; this}(address user, uint amount) {
-        // ... adjust balances
-        return true;
+    @public
+    @payable
+    void deposit{BOT >> this; this}() {
+        balances[msg.sender] = balances[msg.sender] + msg.value;
     }
 }
---
+++ SCIFex/necLines.md
@@ -0,0 +1,16 @@
+DistributedBankEx.scif:
+    8 -> 6 (if default func signature are high integrity)
+A0: 7 -> 5
+W0: 7 -> 5
+W1: 7 -> 5
+
+Uniswap: 3
+
+Token: 4
+
+W0: 3
+
+TownCrier: 6
+W0: 6
+W1: 6
+
