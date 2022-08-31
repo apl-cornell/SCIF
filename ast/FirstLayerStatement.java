32dc801aa4bd278d5d9c72984c7c94af7313011d
Siqiu Yao <yaosiqiu@gmail.com>
2022-08-30 23:23:04 -0400

    clean up

---
+++ ast/FirstLayerStatement.java
@@ -0,0 +1,15 @@
+package ast;
+
+import typecheck.ContractSym;
+
+import java.util.HashSet;
+
+public abstract class FirstLayerStatement extends Statement {
+    
+    public abstract void globalInfoVisit(ContractSym contractSym);
+    /*@Override
+    public void findPrincipal(HashSet<String> principalSet) {
+        // should never be call
+        logger.debug("be called unexpectedly");
+    }*/
+}
